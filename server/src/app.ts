dcf2758f3371895ae9f077f3767f4a5d1fc5a054
Dhruv Ozarkar <realdhruvozarkar@gmail.com>
2026-07-23 19:56:56 +0530

    fixing deployment bug, added type in admindashboard fields

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,8 +7,7 @@ import { env } from "./config/env.js";
 
 const app: express.Application = express();
 
-app.use(cors({ origin: ["http://localhost:5173"] }))
-app.use(express.json());
+app.use(cors({ origin: ["http://localhost:5173", "https://dhruvcardealership.netlify.app"] }))app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/vehicles", vehicleRoutes);
 
